ab679b7110afdd2d9a55c505769c7087dc5883f2
babymaker12 <104803382+babymaker12@users.noreply.github.com>
2025-03-19 02:46:38 +0000

    Lab 1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,11 +1,35 @@
-﻿namespace Lab1;
+﻿using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string name = "Isaias";
-        Console.WriteLine("Hello, "+ name +"!");
-        Console.WriteLine("Welcome to IT-1050");
+        // Step 1: Basic Output
+        Console.WriteLine("Your Name: [Isaias]");
+        Console.WriteLine("Course Title: IT-1050 - Lab 1");
+
+        // Step 2: Use comments
+        /*
+        Name: [Isaias]
+        Title: [IT-1050 - Lab 1]
+        */
+
+        // Step 3: Declare and Use Variables 
+        int favoriteNumber = 2;
+        string favoriteVideoGame = "Minecraft";
+        double numberOfPi = 3.14;
+        bool usingComputer = true;
+
+        Console.WriteLine("Favorite Number: " + favoriteNumber);
+        Console.WriteLine("Favorite Video Game: " + favoriteVideoGame);
+        Console.WriteLine("The number for pi is: " + numberOfPi);
+        Console.WriteLine("Using a computer: " + usingComputer);
+
+        // Step 5: Use Constants 
+        const string schoolName = "Tri-C";
+        Console.WriteLine("School name is " + schoolName);
+
+        // Step 6: Use Meaningful Identifiers 
+        // All the variables above are meaningful  
     }
 }
--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc090cbd7abae86535c6e430d42fb8cffe401e2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b6bcd56fb362605944ac5d9a175140d816108ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
